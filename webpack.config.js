d0866b49668cc4f5466c572b468067991dcf21e9
FrankNewII <franknew2009@gmail.com>
2019-03-25 04:29:14 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,12 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    mode: "development",
+    optimization: {
+        minimize: false
+    },
     devServer: {
         contentBase: path.join(__dirname, 'src'),
-        compress: true,
         port: 9000
     },
     plugins: [
